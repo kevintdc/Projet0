33f261f2db1c4d6f44f91e51b7c79e920c3d51ea
Kevin Toudic <toudickevin@gmail.com>
2025-09-23 09:36:29 +0200

    Déplacement des balises meta de document a index

--- pages/_document.js
+++ pages/_document.js
@@ -4,28 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>
-        Toudic Development | Création de sites web & design freelance
-      </title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Solutions web et design freelance" />
-      <meta name="author" content="Toudic Development" />
-      <meta
-        name="keywords"
-        content="web, design, freelance, développement, création, site internet, graphisme, aude, limoux, 
-        développeur web, carcassonne, aude, meilleur, compétent, qualité, prix, limoux et alentours"
-      />
-      <meta
-        property="og:title"
-        content="Toudic Development | Création de sites web & design freelance"
-      />
-      <meta
-        property="og:description"
-        content="Solutions web et design freelance"
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://toudicdev.fr" />
-      <meta property="og:image" content="/og-image.png" />
+
       <link rel="canonical" href="https://toudicdev.fr" />
       <link rel="icon" href="/favicon.ico" />
       <link
--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,25 @@ function HomeContent() {
         <title>
           Toudic Development | Création de sites web & design freelance
         </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Solutions web et design freelance" />
+        <meta name="author" content="Toudic Development" />
+        <meta
+          name="keywords"
+          content="web, design, freelance, développement, création, site internet, graphisme, aude, limoux, 
+        développeur web, carcassonne, aude, meilleur, compétent, qualité, prix, limoux et alentours"
+        />
+        <meta
+          property="og:title"
+          content="Toudic Development | Création de sites web & design freelance"
+        />
+        <meta
+          property="og:description"
+          content="Solutions web et design freelance"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://toudicdev.fr" />
+        <meta property="og:image" content="/og-image.png" />
       </Head>
 
       <Navbar />
